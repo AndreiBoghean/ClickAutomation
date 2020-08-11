4ac19e559c8eae529c622a55ee85e89353001b53
Andrei <andreiboghean00@gmail.com>
2020-08-11 23:31:53 +0100

    Add project files.

---
+++ ClickAutomation/DllMethods.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Numerics;
+using System.Reflection.Emit;
+
+namespace ClickAutomation
+{
+    
+    public static class DllMethods
+    {
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int dwflags, int dx = 0, int dy = 0, int dwData = 0, int dwExtraInfo = 0);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+
+        /*
+        public static void SetCursorPos(dynamic x, dynamic y)
+        {
+            SetCursorPos(Convert.ToInt32(x), Convert.ToInt32(y));
+        }
+        */
+        [Flags]
+        public enum MouseEventFlags
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010
+        }
+
+        public static void Click(MouseEventFlags[] mouseEventFlagsArr, Point Pos)
+        {
+            SetCursorPos(Pos.X, Pos.Y);
+            mouse_event((int)mouseEventFlagsArr[0]);
+            mouse_event((int)mouseEventFlagsArr[1]);
+        }
+        public static void Drag(MouseEventFlags[] mouseEventFlagsArr, Point FirstPos, Point SecondPos)
+        {
+            SetCursorPos(FirstPos.X, FirstPos.Y);
+            mouse_event((int)mouseEventFlagsArr[0]);
+            SetCursorPos(SecondPos.X, SecondPos.Y);
+            mouse_event((int)mouseEventFlagsArr[1]);
+        }
+    }
+}
---
+++ ClickAutomation/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace ClickAutomation
+{
+    class Program
+    {
+        public static List<Point> TouchPoints = new List<Point>();
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("press enter with this window focused to add mouse pos as a point");
+                Console.ReadLine();
+
+                TouchPoints.Add(System.Windows.Forms.Cursor.Position);
+
+                Console.WriteLine("add more? (y / n)");
+                string inp = Console.ReadLine();
+
+                while (true)
+                {
+                    if ((inp.ToLower() == "y") || (inp.ToLower() == "n")) break;
+                    Console.WriteLine("invalid input, retry input (y / n)");
+                }
+                if (inp == "n") break;
+            }
+            Console.WriteLine("what interval do you want to use between each click?");
+            int count = Convert.ToInt32(Regex.Replace(Console.ReadLine(), "^[0-9]", ""));
+            //count = Math.
+
+            Console.WriteLine("press enter to begin click routine");
+            Console.ReadLine();
+
+            foreach (var position in TouchPoints)
+            {
+                System.Threading.Thread.Sleep(2500);
+                DllMethods.Click(new DllMethods.MouseEventFlags[] { DllMethods.MouseEventFlags.LEFTDOWN, DllMethods.MouseEventFlags.LEFTUP }, position);
+            }
+
+            Console.WriteLine("press enter to close...."); Console.ReadLine();
+        }
+    }
+}
